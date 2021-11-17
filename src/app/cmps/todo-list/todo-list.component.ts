95ce3c1344deef0638f669e6ab26404e3df72a05
urigross <urigross@gmail.com>
2021-11-17 15:53:29 +0200

    Added material design. Added draggble sortable todos ability

--- src/app/cmps/todo-list/todo-list.component.ts
+++ src/app/cmps/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Todo} from 'src/app/models/todo.model';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-todo-list',
@@ -7,12 +9,14 @@ import {Todo} from 'src/app/models/todo.model';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
-  @Input() todos!: Todo[] | null ;
+  @Input() todos!: Todo[] | null;
   // @Input() isEditTodo: boolean = false;
   @Output() onSelectTodo = new EventEmitter<string>();
   @Output() onIdToRemove = new EventEmitter<string>();
   @Output() onToggleIsDone  = new EventEmitter<Todo>();
-
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.todos!, event.previousIndex, event.currentIndex);
+  }
   trackByFn(idx: any, todo: Todo) {
     return todo._id;
   }
